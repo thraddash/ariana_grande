c506f9eefb40e85dfc76ece3d306290ebdce5457
Thraddash <theartist@gmail.com>
2018-11-25 17:46:47 -0800

    stable version

---
+++ src/components/header_footer/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
+
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+              header
+            </div>
+        );
+    }
+}
+
+export default Header;
